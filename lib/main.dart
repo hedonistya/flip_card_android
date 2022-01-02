de1c5fec908f55c1a908eb149dfb835d0891f418
Nikita Topolskov <nikitatopolskov150@gmail.com>
2022-01-02 22:06:06 +0300

    Completed project

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isBack = true;
+  double angle = 0;
+
+  void _flip() {
+    setState(() {
+      angle = (angle + pi) % (2 * pi);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff292a3e),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: _flip,
+                child: TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: angle),
+                    duration: Duration(seconds: 1),
+                    builder: (BuildContext context, double val, __) {
+                      if (val >= (pi / 2)) {
+                        isBack = false;
+                      } else {
+                        isBack = true;
+                      }
+                      return (Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..rotateY(val),
+                        child: Container(
+                            width: 309,
+                            height: 474,
+                            child: isBack
+                                ? Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                          image: AssetImage("assets/back.png")),
+                                    ),
+                                  )
+                                : Transform(
+                                    alignment: Alignment.center,
+                                    transform: Matrix4.identity()..rotateY(pi),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        image: DecorationImage(
+                                          image: AssetImage("assets/face.png"),
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "Surprise!",
+                                          style: TextStyle(fontSize: 30.0),
+                                        ),
+                                      ),
+                                    ),
+                                  ) //else we will display it here,
+                            ),
+                      ));
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
